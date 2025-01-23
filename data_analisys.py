f8ebb791152cfb16f8aff9b79730ea6495d9328a
fdsig <fdesigley@gmail.com>
2025-01-23 22:41:38 +0000

    adding rounding to data

--- data_analisys.py
+++ data_analisys.py
@@ -7,10 +7,18 @@ with open('./speedtests.json', 'r') as f:
 data = [
         {k:v for k,v in kv.items() if k in ['download', 'upload', 'ping', 'ssid']} for kv in data
 ]
+print(data)
+# drop unknown ssid
+data = [i for i in data if i.get('ssid') is not None and i['ssid'] != 'unknown']
 
 df = pd.DataFrame(data)
-
 df = df.groupby('ssid').agg({'download': 'mean', 'upload': 'mean', 'ping': 'mean'})
 
+# 1 decimal place and convert to MB/s
+df.download = df.download.round(1)
+df.upload = df.upload.round(1)
+df.ping = df.ping.round(1)
+# change ssid to [f'ssid_{i}' for i in range(len(df))]
+#df.index = ['Rasberry_pi', 'NanoPi_R2S', 'Home_router']
 
 print(df)
--- speed_test.py
+++ speed_test.py
@@ -6,7 +6,6 @@ def speed_test():
     # get ssid
     # get os (mac, linux, windows)
     os = platform.system()
-    print(os)
     if os.lower() in ['darwin']:
         process = subprocess.Popen(['networksetup', '-listallhardwareports'], stdout=subprocess.PIPE)
         out, err = process.communicate()
@@ -14,28 +13,22 @@ def speed_test():
         # read lines to list
         lines = out.decode('utf-8').split('\n')
         for idx, line in enumerate(lines):
-            print(line)
             if 'Wi-Fi' in line:
-                print(line)
                 device = lines[idx + 1].split(' ')[1]
-                print(device)
                 process = subprocess.Popen(['networksetup', '-getairportnetwork', device], stdout=subprocess.PIPE)
                 out, err = process.communicate()
                 process.wait()
                 ssid = out.decode('utf-8').split('Current Wi-Fi Network: ')[1].split('\n')[0]
-                print(ssid)
     elif os.lower() in ['linux']:
         process = subprocess.Popen(['iwgetid', '-r'], stdout=subprocess.PIPE)
         out, err = process.communicate()
         process.wait()
         ssid = out.decode('utf-8').split('SSID: ')[1].split('\n')[0]
-        print(ssid)
     elif os in ['Windows']:
         process = subprocess.Popen(['netsh', 'wlan', 'show', 'interfaces'], stdout=subprocess.PIPE)
         out, err = process.communicate()
         process.wait()
         ssid = out.decode('utf-8').split('SSID  ')[1].split('\n')[0]
-        print(ssid)
     else:
         ssid = "unknown"
     speedtest_path = Path("./speedtests.json")
@@ -46,16 +39,14 @@ def speed_test():
 
     with open(speedtest_path, "r") as f:
         speedtest_json = json.load(f)
-    
+    # unit is MB/s
     result = subprocess.run(["speedtest-cli", "--secure", "--json"], capture_output=True, text=True)
-    print(result.stdout)
     data = json.loads(result.stdout)
     data['ssid'] = ssid
     speedtest_json.append(data)
     with open(speedtest_path, "w") as f:
         json.dump(speedtest_json, f)
     
-
 speed_test()
 
 
